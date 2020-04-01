e19b41ae8c6270e82ebe70aa069b35a8aba24b29
curiosity654 <curiosity654@outlook.com>
2020-04-01 22:53:19 +0800

    change url

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/5189ae295af4451787bf9a6e4e0e2857?v=6a04fdb853ff4d7e96ad8276086e6c58"
+                configTable: "https://www.notion.so/0cd1317c4bd141a6b12c18d728fc383e?v=515a4ea1771143a385ac60357ee1981d"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/65b35da1762e4f259904bb4cc38e54fd?v=89e80623e52e4419a35f20e54056cfb8"
+                configTable: "https://www.notion.so/5189ae295af4451787bf9a6e4e0e2857?v=6a04fdb853ff4d7e96ad8276086e6c58"
             }
         }
     ],
